6dd075b28213bc531239fea9300298bff83489b4
fabricio-joao <fabriciojoao41@outlook.com>
2022-12-16 10:07:31 -0300

    Manipulando Banco H2

--- src/main/java/com/estudo/springBoot/recursos/UsuarioRecursos.java
+++ src/main/java/com/estudo/springBoot/recursos/UsuarioRecursos.java
@@ -1,19 +1,33 @@
 package com.estudo.springBoot.recursos;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.estudo.springBoot.UsuarioServicos;
 import com.estudo.springBoot.entidades.Usuario;
 
 @RestController 
 @RequestMapping(value = "/usuarios")
 public class UsuarioRecursos {
 	
+	@Autowired
+	UsuarioServicos us;
+	
 	@GetMapping
-	public ResponseEntity<Usuario> buscarUsuarios(){
-	 Usuario usuario = new Usuario(1L, "Jose Agostinho", "jose@gmail.com", "319943510231", "99pi7");
-	 return ResponseEntity.ok().body(usuario);
+	public ResponseEntity<List<Usuario>> buscarUsuarios(){
+	List<Usuario> lista = us.buscarPorUsuarios();
+	 return ResponseEntity.ok().body(lista);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id){
+		Usuario obj = us.buscarPorId(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
